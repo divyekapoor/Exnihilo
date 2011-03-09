acfdae9287a5d7ecf6d71ae5d9f3db057c810755
Divye Kapoor <divyekapoor@gmail.com>
2011-03-09 15:18:46 +0530

    Use profile template to render a profile view (in progress).

--- exnihilo/signup/views.py
+++ exnihilo/signup/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from exnihilo.signup.forms import UserProfileCreationForm, UserProfileModelForm
 
@@ -29,16 +30,16 @@ def profile(request, user_name):
     """
         Returns a profile page for the logged in User (or other User if username provided)
     """
-    ctx = {'title':None,'profile':None}
-    if user_name == "":
+    ctx = {'title':None,'p_user':None} # puser - User object of the requested profile
+    if user_name is None or user_name == "":
         if request.user.is_authenticated():
-            ctx['profile'] = request.user
+            ctx['p_user'] = request.user
         else:
             return HttpResponseNotFound("The user profile that you've requested does not exist.")
     else:
-        ctx['profile'] = get_object_or_404(User, username=user_name)
+        ctx['p_user'] = get_object_or_404(User, username=user_name)
 
-    ctx['title'] = ctx['profile'].get_full_name()
+    ctx['title'] = ctx['p_user'].get_full_name()
     return render_to_response('profile.html', ctx, context_instance=RequestContext(request))
             
         
