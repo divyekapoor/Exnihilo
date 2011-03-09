d2184c7d8bc7b68946769fda7d833a1038f8f26b
Divye Kapoor <divyekapoor@gmail.com>
2011-03-09 15:15:00 +0530

    Implement a photo form and photo model.

--- exnihilo/contest/models.py
+++ exnihilo/contest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Photo(models.Model):
+    
+    author = models.ForeignKey(User)
+    image = models.ImageField(upload_to="upload/photos/%Y/%m/%d", max_length=1000)
+    blurb = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.blurb
--- exnihilo/contest/views.py
+++ exnihilo/contest/views.py
@@ -2,17 +2,35 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+
+from forms import PhotoForm
 
 def index(request):
+    # Home Page
     ctx = {'title' : 'Home'}
     return render_to_response('index.html', ctx, context_instance=RequestContext(request))
     
 def gallery(request):
+    # Gallery page
     ctx = {'title' : 'Gallery'}
     return render_to_response('gallery.html', ctx, context_instance=RequestContext(request))
     
 # TODO
+@login_required
 def submit(request):
-    ctx = {'title' : 'Submit'}
-    return HttpResponse("Submit")
+    # Submission to gallery page
+    ctx = {'title':'Submit', 'form':None}
+    if request.method == "POST":
+        form = PhotoForm(request)
+        if form.is_valid():
+            photo = form.save()
+            ctx['form'] = form
+            ctx['submission_url'] = photo.get_url()
+            return render_to_response('submit_done.html', ctx, context_instance=RequestContext(request))
+    else:
+        form = PhotoForm()
+    
+    ctx['form'] = form
+    return render_to_response('submit.html', ctx, context_instance=RequestContext(request))
     
