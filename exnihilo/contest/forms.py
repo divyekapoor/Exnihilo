ee75b3bb35178d829f991927291cc768638c291d
Divye Kapoor <divyekapoor@gmail.com>
2011-03-09 21:04:02 +0530

    Override the ModelForm for Photo to use a TextArea for the blurb (description) text.

--- exnihilo/contest/forms.py
+++ exnihilo/contest/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from models import Photo
 
 class PhotoForm(forms.ModelForm):
+    blurb = forms.CharField(label="Description", widget=forms.Textarea())
     class Meta:
         model = Photo
+        exclude = ('author',)
         
