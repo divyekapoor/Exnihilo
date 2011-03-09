c63180eec437aa38d0de19e8d2308bd117f54a98
Divye Kapoor <divyekapoor@gmail.com>
2011-03-09 15:19:19 +0530

    Implement gallery and fix profile pic upload folder.

--- exnihilo/signup/models.py
+++ exnihilo/signup/models.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 class UserProfile(models.Model):
     
     user = models.OneToOneField(User)
-    pic = models.ImageField(upload_to="uploads", max_length=1000, verbose_name = "Profile Pic")
+    pic = models.ImageField(upload_to="profile_pics", max_length=1000, verbose_name = "Profile Pic")
     about_me = models.CharField(max_length=1000)
     
     def __unicode__(self):
